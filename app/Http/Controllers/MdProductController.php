fbf2f7fdd9439d6020c008b2b6053951ab8c6158
fahim8921 <fahim.bilal.ch@gmail.com>
2023-08-24 23:44:34 -0700

    faheem

--- app/Http/Controllers/MdProductController.php
+++ app/Http/Controllers/MdProductController.php
@@ -36,17 +36,41 @@ class MdProductController extends Controller
     public function store(Request $request)
     {
         $data = new MdProduct();
-        $data->product_code = $request->product_code;
-        $data->product_name = $request->product_name;
-        $data->product_price = $request->product_price;
-        $data->md_product_category_id = $request->md_product_category_id;
-        $data->cd_client_id = $request->cd_client_id;
-        $data->cd_brand_id = $request->cd_brand_id;
-        $data->cd_branch_id = $request->cd_branch_id;
-        $data->is_active = $request->is_active ?? '1';
-        $data->created_by = $request->created_by;
-        $data->updated_by = $request->updated_by;
-        $data->td_tax_category_id = $request->td_tax_category_id;
+        // $data->product_code = $request->product_code;
+        // $data->product_name = $request->product_name;
+        // $data->product_price = $request->product_price;
+        // $data->md_product_category_id = $request->md_product_category_id;
+        // $data->cd_client_id = $request->cd_client_id;
+        // $data->cd_brand_id = $request->cd_brand_id;
+        // $data->cd_branch_id = $request->cd_branch_id;
+        // $data->is_active = $request->is_active ?? '1';
+        // $data->created_by = $request->created_by;
+        // $data->updated_by = $request->updated_by;
+        // $data->td_tax_category_id = $request->td_tax_category_id;
+        $data->product_code = $request->input('product_code');
+        $data->product_name = $request->input('product_name');
+        $data->product_price = $request->input('product_price');
+        $data->barcode = $request->input('barcode');
+        $data->md_station_id = $request->input('md_station_id');
+        $data->maximun_day_of_product_return = $request->input('maximun_day_of_product_return');
+        $data->cooking_time = $request->input('cooking_time');
+        $data->description = $request->input('description');
+        $data->md_allergy_id = $request->input('md_allergy_id');
+        $data->md_diet_id = $request->input('md_diet_id');
+        $data->gift = $request->input('gift');
+        $data->portion = $request->input('portion');
+        $data->bundle = $request->input('bundle');
+        $data->not_allow_apply_discount = $request->input('not_allow_apply_discount');
+        $data->sold_by_weight = $request->input('sold_by_weight');
+        $data->sale_price = $request->input('sale_price');
+        $data->md_product_category_id = $request->input('md_product_category_id');
+        $data->td_tax_category_id = $request->input('td_tax_category_id');
+        $data->cd_client_id = $request->input('cd_client_id');
+        $data->cd_brand_id = $request->input('cd_brand_id');
+        $data->cd_branch_id = $request->input('cd_branch_id');
+        $data->is_active = $request->input('is_active', '1');
+        $data->created_by = $request->input('created_by');
+        $data->updated_by = $request->input('updated_by');
 
         if ($image = $request->file('product_image')) {
             $destinationPath = public_path('img/product_image/');
@@ -83,17 +107,30 @@ class MdProductController extends Controller
     {
         //
         $data =  MdProduct::find($id);
-        $data->product_code = $request->product_code;
-        $data->product_name = $request->product_name;
-        $data->product_price = $request->product_price;
-        $data->md_product_category_id = $request->md_product_category_id;
-        $data->cd_client_id = $request->cd_client_id;
-        $data->cd_brand_id = $request->cd_brand_id;
-        $data->cd_branch_id = $request->cd_branch_id;
-        $data->is_active = $request->is_active;
-        $data->created_by = $request->created_by;
-        $data->updated_by = $request->updated_by;
-        $data->td_tax_category_id = $request->td_tax_category_id;
+        $data->product_code = $request->input('product_code');
+        $data->product_name = $request->input('product_name');
+        $data->product_price = $request->input('product_price');
+        $data->barcode = $request->input('barcode');
+        $data->md_station_id = $request->input('md_station_id');
+        $data->maximun_day_of_product_return = $request->input('maximun_day_of_product_return');
+        $data->cooking_time = $request->input('cooking_time');
+        $data->description = $request->input('description');
+        $data->md_allergy_id = $request->input('md_allergy_id');
+        $data->md_diet_id = $request->input('md_diet_id');
+        $data->gift = $request->input('gift');
+        $data->portion = $request->input('portion');
+        $data->bundle = $request->input('bundle');
+        $data->not_allow_apply_discount = $request->input('not_allow_apply_discount');
+        $data->sold_by_weight = $request->input('sold_by_weight');
+        $data->sale_price = $request->input('sale_price');
+        $data->md_product_category_id = $request->input('md_product_category_id');
+        $data->td_tax_category_id = $request->input('td_tax_category_id');
+        $data->cd_client_id = $request->input('cd_client_id');
+        $data->cd_brand_id = $request->input('cd_brand_id');
+        $data->cd_branch_id = $request->input('cd_branch_id');
+        $data->is_active = $request->input('is_active', '1');
+        $data->created_by = $request->input('created_by');
+        $data->updated_by = $request->input('updated_by');
 
 
         if ($image = $request->file('product_image')) {
